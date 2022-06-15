04208ccf416160b1248d23557a7fe6396ab5e697
peltonan <peltonen.annette@gmail.com>
2022-06-15 14:52:52 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,42 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+      );
 
-  useEffect(() => {
-    const transformTasks = (tasksObj) => {
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
       const loadedTasks = [];
 
-      for (const taskKey in tasksObj) {
-        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    };
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
 
-    fetchTasks(
-      { url: 'https://exercise-four-peltonan-default-rtdb.europe-west1.firebasedatabase.app/tasks.json' },
-      transformTasks
-    );
-  }, [fetchTasks]);
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
